ba88241fd18df18a7d7510c382740644f62a25ce
Dima <strogansev.dima@yandex.ru>
2024-10-23 23:49:45 +0300

    Рефакторинг класса PlayerScoreTennis

--- src/service/match_scoreboard_logic/player_score_tennis.py
+++ src/service/match_scoreboard_logic/player_score_tennis.py
@@ -1,22 +1,24 @@
 class PlayerScoreTennis:
+
     SCORE_MAPPING_GAME = {
         0: 0, "15": 1, "30": 2, "40": 3, "AD": 4
     }
 
     def __init__(self, name, score=None):
+        self.name = name
         if score is not None:
-            number_game_score = self.SCORE_MAPPING_GAME[score[name]["game_score"]]
-            self.name = name
-            self.game_score = int(number_game_score)
+            self.game_score = self.convert_game_score(score[name]["game_score"])
             self.tie_break_score = score[name]["tie_break_score"]
             self.set_score = score[name]["set_score"]
             self.match_score = score[name]["match_score"]
         else:
-            self.name = name
             self.game_score = 0
             self.tie_break_score = 0
             self.set_score = 0
             self.match_score = 0
+
+    def convert_game_score(self, game_score):
+        return self.SCORE_MAPPING_GAME[game_score]
 
     def to_dict(self):
         return {
